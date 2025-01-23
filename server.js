1a2435295efd68ff98afcc49b27ed3f214bde835
aditya-varma8019 <rvenkataditya@gmail.com>
2025-01-23 15:04:04 +0530

    change in server.js

--- server.js
+++ server.js
@@ -43,19 +43,19 @@ app.use('/api/v1/auth', authRoute);
 app.use('/api/v1/category', categoryRoutes);
 app.use('/api/v1/product', productRoutes);
 
-const __dirname1 = path.resolve();
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname1, '/client/build')))
+// const __dirname1 = path.resolve();
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname1, '/client/build')))
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname1, 'client', 'build', 'index.html'))
-    })
-} else {
-    app.get('/', (req, res) => {
-        res.send('API is running...')
-    })
-}
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname1, 'client', 'build', 'index.html'))
+//     })
+// } else {
+// }
 
+app.get('/', (req, res) => {
+    res.send('API is running...')
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
